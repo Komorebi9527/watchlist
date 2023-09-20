10d5baf4d3cff77b09146d89178b3612f86096f7
Komorebi <1037108348@qq.com>
2023-09-20 13:30:15 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -9,47 +9,77 @@
 
 #(Flask) D:\PycharmProjects\watchlist>flask run      在命令行输入
 from flask import Flask,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
+
+
+WIN=sys.platform.startswith('win')
+if WIN:# 如果是 Windows 系统，使用三个斜线
+    prefix='sqlite:///'
+else:# 否则使用四个斜线
+    prefix='sqlite:////'
+
 app=Flask(__name__)
-# @app.route('/Komorebi')
-# @app.route('/')
-# @app.route('/home')
-# def Hello():
-#     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-#
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return '你好，初次见面，请多多指教！'
-#
-#
-# @app.route('/test')
-# def test_url_for():
-#     print(url_for('Hello'))
-#     print(url_for('user_page',name='Komorebi'))
-#     print(url_for('test_url_for'))
-#     return url_for('user_page',name='Komorebi')
-#
-# @app.route('/<any(student,class):url_path>/<id>/')
-# def item(url_path, id):
-#     if url_path == 'student':
-#         return '学生{}详情'.format(id)
-#     else:
-#         return '班级{}详情'.format(id)
+#写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址
+#sqlite:////数据库文件的绝对地址
+#使用 Windows 系统，上面的 URI 前缀部分需要写入三个斜线（即sqlite:/// ）
+app.config['SQLALCHEMY_DATABASE_URI']=prefix+os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #关闭对模型修改的监控
+# # 在扩展类实例化前加载配置
+db=SQLAlchemy(app)
 
 
-name = 'Komorebi'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+#创建用户信息表
+class User(db.Model):# 表名将会是 user（自动生成，小写处理）
+    id=db.Column(db.Integer,primary_key=True)# 主键
+    name=db.Column(db.String(20))# 名字
+#创建电影名称表
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year=db.Column(db.String(4))#年份
 
+
+@app.cli.command()# 注册为命令
+@click.option('--drop',is_flag=True,help='Create after drop.')
+# 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:# 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialize the database')#输出提示信息
+
+
+#创建自定义命令 forge
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    name = 'Komorebi'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user=User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie=Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done')
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    user=User.query.first()#读取用户信息
+    movies=Movie.query.all()#读取电影信息
+    return render_template('index.html',user=user,movies=movies)
